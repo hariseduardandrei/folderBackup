24741acb86684d273ea06e9112d1fe3d9830870c
Eduard Haris <eduard.haris@esolutions.ro>
2018-11-19 17:23:17 +0200

    Paul's version

--- main.cpp
+++ main.cpp
@@ -1,62 +1,135 @@
+#include <iostream>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-bool is_directory(const char *entry_path);
-
-void list_directory(const char *source_directory);
+bool is_dir(const char* path);
+void copyFile_(string inDir, string outDir);
+void copyDir_(const char *inputDir, string outDir);
 
 int main()
 {
-    string source_directory;
 
-    // cout << "Calea directorului: ";
-    // getline(cin, source_directory);
-
-    source_directory = "/Users/eduard/Desktop/ospatuldepiatra";
-
-    list_directory(source_directory.c_str());
+    cout << "Calea directorului sursa: ";
+    string srcDir;
+    getline(cin, srcDir); // Get from the keyboard the source path
+    cout << "Calea directorului destinatie: ";
+    string destDir;
+    getline(cin, destDir); // Get from the keyboard the destination path
+    copyDir_(srcDir.c_str(),  destDir); // first call for the function copyDir_
 
     return 0;
+
+
+
 }
 
-void list_directory(const char *source_directory)
+bool is_dir(const char* path) // Check if the path is a directory or a file
 {
-    DIR *directory;
-    struct dirent *entry_interator;
-    string entry_path, entry_name, source_directory_copy;
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISDIR(buf.st_mode);
+}
 
-    if (is_directory(source_directory) == false)
+bool is_(const char* path) // checks if the path exists in the destination folder
+{
+    struct stat buf;
+    return stat(path, &buf);
+}
+
+void copyFile_(const char* inDir,const char* outDir){
+    string ind = inDir;
+    string outd = outDir;
+    if ( is_( outd.c_str() ) == 0 ) // If the file exists
     {
+        struct stat out;
+        struct stat in;
+        stat(inDir, &in);
+        stat(outDir, &out);
+        if(in.st_mtime>out.st_mtime || in.st_size>out.st_size){ // if the last modify of the source file is higher than the destination
+            DeleteFile(outd.c_str());
+            CopyFile(ind.c_str(), outd.c_str(), 1); // delete and copy the new file
+            cout << "--> Updated" ;
+        }
+        cout << "\n";
+    }else{
+        CopyFile(ind.c_str(),outd.c_str(),1); // only copy file cause it doesn't exist
+    }
+}
+
+void copyDir_(const char *inputDir, string outDir)
+{
+
+    DIR *pDIR;
+    struct dirent *entry;
+    string tmpStr, tmpStrPath, outStrPath, inputDir_str = inputDir, outDIR_str = outDir, tmpOut;
+
+    if (is_dir(inputDir) == false) // if the given dir at the start is a file it stops
+    {
+        cout << "This is not a folder " << endl;
         return;
     }
 
-    if ((directory = opendir(source_directory)))
+    if( pDIR = opendir(inputDir_str.c_str()) ) // Open directory "inputDir" (changes in base of the function calls)
     {
-        while ((entry_interator = readdir(directory)))
+
+        while(entry = readdir(pDIR)) // get folders and files names
         {
-            entry_name = entry_interator->d_name;
-            if (strcmp(entry_name.c_str(), ".") != 0 && strcmp(entry_name.c_str(), "..") != 0)
+            tmpStr = entry->d_name;
+
+            if( strcmp(entry->d_name, ".")  != 0 && strcmp(entry->d_name, "..") != 0 )
             {
-                cout << entry_name<<endl;
-                source_directory_copy = source_directory;
-                entry_path = source_directory_copy.append("/").append(entry_name);
-                if (is_directory(entry_path.c_str()))
+                tmpStrPath = inputDir_str;
+                tmpStrPath.append( "\\" );
+                tmpStrPath.append( tmpStr ); // Add file/folder name to the source "URL"
+                tmpOut = outDIR_str;
+                tmpOut.append( "\\" );
+                tmpOut.append( tmpStr ); // Add file/folder name to the destination "URL"
+
+                if ( is_( tmpOut.c_str() ) == 0 ) // Test if file/folder exists
                 {
-                    list_directory(entry_path.c_str());
+                    cout << entry->d_name;
+                    if (is_dir(tmpStrPath.c_str())) // Test if is a file or a folder
+                    {
+                        cout << "\n";
+                        copyDir_(tmpStrPath.c_str(), tmpOut); // Is a folder so we only open it
+                    }else{
+                        outStrPath = outDir;
+                        outStrPath.append( "\\" );
+                        outStrPath.append( tmpStr );
+                        copyFile_(tmpStrPath.c_str(), outStrPath.c_str());  // Is a file so we in case it's different we update it
+                    }
+                }
+                else
+                {
+                    cout << entry->d_name;
+                    if (is_dir(tmpStrPath.c_str()))
+                    {
+                        cout << "--> Folder Created" << "\n";
+                        // Create Folder on the destination path
+                        outStrPath = outDir;
+                        outStrPath.append( "\\" );
+                        outStrPath.append( tmpStr );
+                        mkdir(outStrPath.c_str()); // Create directory with the same name of the source
+
+                        copyDir_(tmpStrPath.c_str(), outStrPath); // Give to this SAME function the new path to copy files in the new directory
+                    }
+                    else
+                    {
+                        cout << "--> File Copied"   << "\n";
+                        // copy file on the destination path
+                        outStrPath = outDir;
+                        outStrPath.append( "\\" );
+                        outStrPath.append( tmpStr );
+
+                        copyFile_(tmpStrPath.c_str(), outStrPath.c_str()); // copy file to backup folder
+                    }
                 }
             }
         }
-        closedir(directory);
+        closedir(pDIR); // closing dirent pointer to the source folder
     }
-}
-
-bool is_directory(const char *entry_path)
-{
-    struct stat path_status;
-    stat(entry_path, &path_status);
-    return S_ISDIR(path_status.st_mode);
 }
